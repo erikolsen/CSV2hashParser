11d0a69826215e437d5529e2912e7af56f89c803
Erik Olsen <erik.s.olsen@gmail.com>
2014-07-27 16:12:28 -0500

    converted tabs to spaces

--- parser.rb
+++ parser.rb
@@ -1,26 +1,26 @@
 require 'CSV'
 
 class ParseFile
-	attr_reader :csv_file
-	def initialize(file = {})
-		@csv_file = file[:csv_file]
-	end
+  attr_reader :csv_file
+  def initialize(file = {})
+    @csv_file = file[:csv_file]
+  end
 
 
-	def display_csv_lines_as_arrays
-		CSV.foreach(csv_file) do |row|
-			puts row.inspect
-		end
-	end
+  def display_csv_lines_as_arrays
+    CSV.foreach(csv_file) do |row|
+      puts row.inspect
+    end
+  end
 
 
-	def parse_csv_to_hash
-		hash_data_array = []
-		CSV.table(csv_file).each do |row|
-			hash_data_array << row.to_hash
-		end
-		return hash_data_array
-	end
+  def parse_csv_to_hash
+    hash_data_array = []
+    CSV.table(csv_file).each do |row|
+      hash_data_array << row.to_hash
+    end
+    return hash_data_array
+  end
 
 end#class end
 
